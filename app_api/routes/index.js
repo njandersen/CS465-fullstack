a2b44ad5faaf0f75385dc08a563fcfa778aca62f
njandersen <njandersen22@gmail.com>
2022-10-09 22:08:46 -0400

    add angular spa

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 const tripsController = require('../controllers/trips');
 
-router.route('/trips').get(tripsController.tripsList);
+router
+  .route('/trips')
+  .get(tripsController.tripsList)
+  .post(tripsController.tripsAddTrip)
+  .put(tripsController.tripsUpdateTrip);
 
 router.route('/trips/:tripCode').get(tripsController.tripsFindCode);
 
